fc342cf8b61f08497560e9681d92d9bb27487547
Chingiz Baibogirov <chingiz@avana.asia>
2020-11-20 10:17:42 +0600

    welcome page

--- views/site/index.php
+++ views/site/index.php
@@ -14,11 +14,19 @@ $this->title = 'Lottery Application';
         </div>
     <?php endif; ?>
 
+    <?php if(!Yii::$app->user->isGuest): ?>
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
         <p class="lead">You have the opportunity to receive a gift )))</p>
-
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get Gift ...</a></p>
     </div>
+    <?php else: ?>
+        <div class="jumbotron">
+            <h1>Welcome to Lottery</h1>
+
+            <p class="lead">Pass the registration or <a href="<?php echo Url::to(['/site/login']); ?>">sign in</a> to get the gift!</p>
+            <p><a class="btn btn-lg btn-success" href="<?php echo Url::to(['/sign-up/index']); ?>">Sign Up Now</a></p>
+        </div>
+    <?php endif; ?>
 </div>
